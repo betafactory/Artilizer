8b66b817e6ad80a3c5c2e1eca4fe211f8973db6f
S Rahul Badami <srahulbadami@gmail.com>
2019-11-14 15:40:42 +0530

    Configured Procfile for Deployment

--- artilizer/wsgi.py
+++ artilizer/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artilizer.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
